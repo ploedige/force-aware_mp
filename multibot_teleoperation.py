29863d9244153d956a69f475392da30f44a54a79
paul-loedige <paul@gve-loedige.de>
2024-03-04 15:21:44 +0100

    imitator -> replicant. Allowing for multiple replicants

--- multibot_teleoperation.py
+++ multibot_teleoperation.py
@@ -1,5 +1,6 @@
 import hydra
 import logging
+from typing import List
 
 from torchcontrol.policies import HybridJointImpedanceControl
 from polymetis import RobotInterface
@@ -9,23 +10,18 @@ from controllers.human_controller import HumanController
 class TeleoperationHandler:
     def __init__(self, 
                  demonstrator: RobotInterface,
-                 imitator: RobotInterface):
-        """handler for using two robots in teleoperation
-
-        Args:
-            demonstrator (RobotInterface): the robot used for demonstration
-            imitator (RobotInterface): the robot that replays the demonstrator's movements
-        """
+                 replicants: List[RobotInterface]):
         self.logger = logging.getLogger('teleoperation_handler')
         self.demonstrator = demonstrator
-        self.imitator = imitator
+        self.replicants = replicants
         self.sync_robot_positions()
 
     def sync_robot_positions(self):
         """Sync the robot positions by moving the imitator to the current position of the demonstrator"""
         self.logger.info(f"Moving Imitator to Demonstrator's joint positions")
         demonstrator_pos = self.demonstrator.get_joint_positions()
-        self.imitator.move_to_joint_positions(demonstrator_pos)
+        for replicant in self.replicants:
+            replicant.move_to_joint_positions(demonstrator_pos)
 
     def run_teleoperation(self):
         """teleoperate the robots until a Keyboard interrupt is received
@@ -34,20 +30,22 @@ class TeleoperationHandler:
         self.logger.info("Initializing Policies")
         demonstrator_policy = HumanController(self.demonstrator.robot_model)
         self.demonstrator.send_torch_policy(demonstrator_policy, blocking=False)
-        imitator_policy = HybridJointImpedanceControl(joint_pos_current=self.imitator.get_joint_positions(),
-                                                      Kq=self.imitator.Kq_default, 
-                                                      Kqd=self.imitator.Kqd_default, 
-                                                      Kx=self.imitator.Kx_default, 
-                                                      Kxd=self.imitator.Kxd_default, 
-                                                      robot_model=self.imitator.robot_model,
-                                                      ignore_gravity=self.imitator.use_grav_comp)
-        self.imitator.send_torch_policy(imitator_policy, blocking=False)
+        for replicant in self.replicants:
+            replicant_policy = HybridJointImpedanceControl(joint_pos_current=replicant.get_joint_positions(),
+                                                        Kq=replicant.Kq_default, 
+                                                        Kqd=replicant.Kqd_default, 
+                                                        Kx=replicant.Kx_default, 
+                                                        Kxd=replicant.Kxd_default, 
+                                                        robot_model=replicant.robot_model,
+                                                        ignore_gravity=replicant.use_grav_comp)
+            replicant.send_torch_policy(replicant_policy, blocking=False)
 
         self.logger.info("Starting Teleoperation")
         try:
             while True:
                 joint_pos_demonstrator = self.demonstrator.get_joint_positions()
-                self.imitator.update_desired_joint_positions(joint_pos_demonstrator)
+                for replicant in self.replicants:
+                    replicant.update_desired_joint_positions(joint_pos_demonstrator)
 
         except KeyboardInterrupt:
             self.logger.info("Received Interrupt Signal. Exiting teleoperation...")
@@ -57,10 +55,10 @@ class TeleoperationHandler:
 def main(cfg):
     demonstrator = RobotInterface(ip_address = cfg.robot_1.server_ip,
                                   port = cfg.robot_1.robot_port)
-    imitator = RobotInterface(ip_address = cfg.robot_2.server_ip,
+    replicant_1 = RobotInterface(ip_address = cfg.robot_2.server_ip,
                               port = cfg.robot_2.robot_port)
 
-    handler = TeleoperationHandler(demonstrator, imitator)
+    handler = TeleoperationHandler(demonstrator, [replicant_1])
     handler.run_teleoperation()
 
 if __name__ == "__main__":
