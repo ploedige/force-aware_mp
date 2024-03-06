650da90efefb2a6ce2520697035aaf39ff6b108b
paul-loedige <paul@gve-loedige.de>
2024-03-06 11:45:36 +0100

    rewrote human and force feedback controllers

--- controllers/force_feedback_controller.py
+++ controllers/force_feedback_controller.py
@@ -8,97 +8,42 @@ from human_controller import HumanController
 from control_modules.kalman_filter import KalmanFilter
 
 class ForceFeedbackController(HumanController):
-    def __init__(self, robot_model:toco.models.RobotModelPinocchio, initial_replica_load: torch.Tensor, 
-                 regularize:bool = True, force_feedback:bool = True):
-        """Controller for human input with force feedback from a replicating robot
+    def __init__(self, 
+                 robot_model: toco.models.RobotModelPinocchio, 
+                 assistive_gain:torch.Tensor = torch.Tensor([0.26, 0.44, 0.40, 1.11, 1.10, 1.20, 0.85]),
+                 centering_gain:torch.Tensor = torch.Tensor([5.0, 2.2, 1.3, 0.3, 0.1, 0.1, 0.0]),
+                 initial_replication_torques: torch.Tensor = torch.Tensor([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]), 
+                 force_feedback_damping_gain: torch.Tensor = torch.Tensor([25.0, 25.0, 25.0, 25.0, 7.5, 4.0, 4.0]),
+                 force_feedback:bool = True):
+        """HumanController with capability to give force feedback to the user
+        To reproduce a torque the torque has to be transmitted to this controller via the update_parameter({"replication_torque": <torques_to_be_reproduced>}) function
 
         Args:
-            robot_model (toco.models.RobotModelPinocchio): model of the controlled robot
-            initial_replica_load (torch.Tensor): initial load from the replicant
-            regularize (bool, optional): Defaults to True.
-            force_feedback (bool, optional): _description_. Defaults to True.
+            robot_model (toco.models.RobotModelPinocchio): physical model of the robot
+            assistive_gain (torch.Tensor, optional): gain for assisting the movement of the robot. Defaults to torch.Tensor([0.26, 0.44, 0.40, 1.11, 1.10, 1.20, 0.85]).
+            centering_gain (torch.Tensor, optional): this gain lightly forces the robot back into its idle position. Set all elements to 0 to disable. Defaults to torch.Tensor([5.0, 2.2, 1.3, 0.3, 0.1, 0.1, 0.0]).
+            initial_replication_torques (torch.Tensor, optional): initialize the force feedback. Defaults to torch.Tensor([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]).
+            force_feedback_damping_gain (torch.Tensor, optional): gain for damping the force feedback depending on the current velocity. Defaults to torch.Tensor([25.0, 25.0, 25.0, 25.0, 7.5, 4.0, 4.0]).
+            force_feedback (bool, optional): enables/disables force feedback. Defaults to True.
         """
-        super().__init__(robot_model, regularize)
-        # Force feedback parameters
-        self.fb_gain = -1.0 * np.array(
-            [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0], dtype=np.float64
-        )
-
-        self.torque_dgain = 0.5 * np.array(
-            [50.0, 50.0, 50.0, 50.0, 15.0, 8.0, 8.0], dtype=np.float64
-        )
-        self.tau_moving_average = np.zeros(7)
-        self.alpha = 0.9
-
-
-        self.replica_load = initial_replica_load
-        self._replica_load_filter = KalmanFilter(self.replica_load)
-
+        super().__init__(robot_model, assistive_gain, centering_gain)
+        self.replication_torques = torch.nn.Parameter(initial_replication_torques)
+        self._force_feedback_damping_gain = force_feedback_damping_gain
         self._force_feedback = force_feedback
 
-    def get_force_feedback(self):
-        """
-        General idea of this torque feedback:
-        Have a D controller on the primary velocity which activates only when external forces are present. This feature
-        combines the safety of the primary robot with its useabilty: if the
-        D controller were always active the human would not be able to move the primary robot easily. However, without
-        a D controller the primary robot (which is next to a human) can move very fast if the external forces are high.
-        This needs to be prevented. This is the algorithm:
-        1. Get the filtered external forces from the replica robot
-        2. filter out small forces, this is crucial to filter out the inherent sensor noise.
-           While we loose the feedback of small forces, this improves the general control of the primary robot.
-           While this also clips the forces with 'max_force', the current D controller on the primary velocity handles
-           too high forces acting on the primary robot as well by regularizing the primary velocity.
-        3. Compute the D control feedback 'd_feedback' based on the primary joint velocities
-        4. clip 'd_feedback' with the absolute value of the current external forces. However, for a safer
-           environment, also include the latest external forces with a moving average.
-        returns the force feedback - 'd_feedback'
-        """
-        # 1. get force feedback from replica
-        replica_load = self._replica_load_filter(self.replica_load)
-        # 2. total force clip filter
-        # computes l1 norm of the replica load (maybe with incorporated d-gain control of the primary velocity).
-        MIN_FORCE_THRESHOLD = 4.0 
-        FORCE_INTERPOLATION_THRESHOLD = 4.66
-        # If it is bigger than a: return replica_load
-        MAX_FORCE  = 60.0# if the force is bigger than MAX_FORCE: scale it down
+    def _force_feedback_torques(self, replication_torques: torch.Tensor, joint_vel_current: torch.Tensor) -> torch.Tensor:
+        feedback_damping = self._force_feedback_damping_gain * joint_vel_current
+        boundaries = torch.abs(replication_torques)
+        feedback_damping = torch.clamp(feedback_damping, -boundaries, boundaries)
+        return replication_torques - feedback_damping
 
-        total_force = np.sum(np.abs(self.fb_gain * replica_load))
-        if total_force < MIN_FORCE_THRESHOLD:
-            plain_feedback =  np.zeros(7)
-        elif total_force < FORCE_INTERPOLATION_THRESHOLD:
-            interpolation_area = FORCE_INTERPOLATION_THRESHOLD - MIN_FORCE_THRESHOLD
-            plain_feedback = (1 / (interpolation_area) * total_force - FORCE_INTERPOLATION_THRESHOLD / (interpolation_area)) * replica_load
-        elif total_force < MAX_FORCE:
-            plain_feedback =  replica_load
-        else:
-            plain_feedback =  replica_load * (MAX_FORCE / total_force)
-
-        # 3. get d control feedback from primary
-        primary_j_vel = self.primary_robot.current_j_vel
-        d_feedback = self.torque_dgain * primary_j_vel
-        # update moving average of the latest external forces
-        self.tau_moving_average = self.alpha * self.tau_moving_average + (
-            1 - self.alpha
-        ) * np.abs(plain_feedback)
-        # 4. compute clipping boundaries for d_feedback
-        m = np.maximum(np.abs(plain_feedback), self.tau_moving_average)
-        # clip and return
-        d_feedback = np.clip(d_feedback, -m, m)
-        return plain_feedback - d_feedback
-
-    def enable_force_feedback(self):
-        print("Enabled Force Feedback!")
-        self._active_force_feedback = True
-
-    def disable_force_feedback(self):
-        print("Disabled Force Feedback!")
-        self._active_force_feedback = False
 
     def forward(self, state_dict: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
-        human_control_torques = super().forward(state_dict)["joint_torques"]
-        force_feedback = self.get_force_feedback()
-        if self._active_force_feedback:
-            return {"joint_torques" : human_control_torques + force_feedback}
+        assistive_torques = self._get_assistive_torques(state_dict["motor_torques_external"])
+        centering_torques = self._get_centering_torques(state_dict["joint_positions"])
+        force_feedback_torques = self._get_force_feedback_torques(self.replication_torques, state_dict["joint_velocities"])
+
+        if self._force_feedback:
+            return {"joint_torques": assistive_torques + centering_torques - force_feedback_torques}
         else:
-            return {"joint_torques" : human_control_torques}
+            return {"joint_torques": assistive_torques + centering_torques}
--- controllers/human_controller.py
+++ controllers/human_controller.py
@@ -5,39 +5,38 @@ from typing import Dict
 
 class HumanController(toco.PolicyModule):
     # stolen from SimulationFramework
-    def __init__(self, robot_model: toco.models.RobotModelPinocchio, regularize:bool = True):
-        """Initializes the human controller
+    def __init__(self, 
+                 robot_model: toco.models.RobotModelPinocchio, 
+                 assistive_gain:torch.Tensor = torch.Tensor([0.26, 0.44, 0.40, 1.11, 1.10, 1.20, 0.85]),
+                 centering_gain:torch.Tensor = torch.Tensor([5.0, 2.2, 1.3, 0.3, 0.1, 0.1, 0.0])):
+        """Controller that assists the moving of a robot by a human
 
         Args:
-            joint_angle_limits (Tensor): joint angle limits of the controlled robot. Needed for regularization
-            regularize (bool, optional): Defaults to True.
+            robot_model (toco.models.RobotModelPinocchio): model that physically describes the robot
+            assistive_gain (torch.Tensor, optional): gain for assisting the movement of the robot. Defaults to torch.Tensor([0.26, 0.44, 0.40, 1.11, 1.10, 1.20, 0.85]).
+            centering_gain (torch.Tensor, optional): this gain lightly forces the robot back into its idle position. Set all elements to 0 to disable. Defaults to torch.Tensor([5.0, 2.2, 1.3, 0.3, 0.1, 0.1, 0.0]).
         """
         super().__init__()
 
         joint_angle_limits = robot_model.get_joint_angle_limits()
-        self.joint_pos_min = joint_angle_limits[0]
-        self.joint_pos_max = joint_angle_limits[1]
+        self._joint_pos_min = joint_angle_limits[0]
+        self._joint_pos_max = joint_angle_limits[1]
 
-        # define gain
-        self.gain = torch.Tensor([0.26, 0.44, 0.40, 1.11, 1.10, 1.20, 0.85])
+        self._assistive_gain = assistive_gain
 
-        if regularize:
-            self.reg_gain = torch.Tensor([5.0, 2.2, 1.3, 0.3, 0.1, 0.1, 0.0])
-        else:
-            self.reg_gain = torch.Tensor([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+        self._centering_gain = centering_gain
+
+    def _get_centering_torques(self, joint_pos_current: torch.Tensor) -> torch.Tensor:
+        left_boundary = 1 / torch.clamp(torch.abs(self._joint_pos_min - joint_pos_current), 1e-8, 100_000)
+        right_boundary = 1 / torch.clamp(torch.abs(self._joint_pos_max - joint_pos_current), 1e-8, 100_000)
+        centering_load = left_boundary - right_boundary
+        return self._centering_gain * centering_load
+
+    def _get_assistive_torques(self, external_torques: torch.Tensor) -> torch.Tensor:
+       return -self._assistive_gain * external_torques
 
     def forward(self, state_dict: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
-        ext = state_dict["motor_torques_external"]
+        assistive_torques = self._get_assistive_torques(state_dict["motor_torques_external"])
+        centering_torques = self._get_centering_torques(state_dict["joint_positions"])
 
-        human_torque = -self.gain * ext
-
-        joint_pos_current = state_dict["joint_positions"]
-
-        left_boundary = 1 / torch.clamp(torch.abs(self.joint_pos_min - joint_pos_current), 1e-8, 100_000)
-        right_boundary = 1 / torch.clamp(torch.abs(self.joint_pos_max - joint_pos_current), 1e-8, 100_000)
-
-        reg_load = left_boundary - right_boundary
-
-        reg_torgue = self.reg_gain * reg_load
-
-        return {"joint_torques": human_torque + reg_torgue}
+        return {"joint_torques": assistive_torques + centering_torques}
