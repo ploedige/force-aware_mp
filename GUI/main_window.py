d09cd7b43d30449e8840048957d8025dfbf88e3d
paul-loedige <paul@gve-loedige.de>
2024-03-12 14:54:25 +0100

    changed config structure (once again)

--- GUI/main_window.py
+++ GUI/main_window.py
@@ -1,12 +1,16 @@
 import tkinter as tk
 from tkinter import font
 
+from omegaconf import DictConfig
+
+from polymetis import RobotInterface
+
 from GUI.robot_control import RobotControl
 from GUI.task_control import TaskControl
 from GUI.logging_control import LoggingControl
 
 class MainWindow(tk.Tk):
-    def __init__(self):
+    def __init__(self, env_cfg:DictConfig):
         super().__init__()
         title = "FAM User Interface"
         self.title(title)
@@ -20,8 +24,8 @@ class MainWindow(tk.Tk):
         log_control = LoggingControl(self)
         log_control.grid(row=2, column=0, padx=0, pady=0)
 
-        demonstrator_control = RobotControl(self, "Test 1")
+        demonstrator_control = RobotControl(self, env_cfg.robots[0])
         demonstrator_control.grid(row=1, column=1, padx=5, pady=5)
 
-        replicant_control = RobotControl(self, "Test 2")
+        replicant_control = RobotControl(self, env_cfg.robots[1])
         replicant_control.grid(row=2, column=1, padx=5, pady=5)
