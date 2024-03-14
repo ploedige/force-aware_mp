c324c4fce74db0cae42eadea3e5e852f09e82af4
paul-loedige <paul@gve-loedige.de>
2024-03-14 12:41:43 +0100

    implemented basic data store manager (untested)

--- data_magement/base_data_manager.py
+++ data_magement/base_data_manager.py
@@ -1,10 +1,12 @@
 import threading
 import logging
+import h5py
+import os
 
 import abc
 
 class BaseDataManager(threading.Thread, abc.ABC):
-    def __init__(self, log_info:str = '', store_freq:float = '0.5'):
+    def __init__(self, log_info:str = '', store_freq:float = 0.5):
         super().__init__()
         self.logger = logging.Logger(__name__)
         self._stop_event = threading.Event()
@@ -17,3 +19,11 @@ class BaseDataManager(threading.Thread, abc.ABC):
     def split(self):
         self._split_event.set()
         self.logger.debug("Split event set.")
+
+    def _store_data(self, data, log_name):
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        data_dir = os.path.join(current_dir, "data")
+        log_file_path = os.path.join(data_dir, f"{log_name}.h5")
+        with h5py.File(log_file_path, 'w') as f:
+            for key, value in data.items():
+                f.create_dataset(key, data=value)
--- data_magement/robot_data_manager.py
+++ data_magement/robot_data_manager.py
@@ -1,13 +1,30 @@
-import threading
-import logging
+from datetime import datetime
 
 from polymetis import RobotInterface
 from data_magement.base_data_manager import BaseDataManager
 
 class RobotDataManager(BaseDataManager):
-    def init__(self, robot: RobotInterface, log_info:str = '', store_freq:float = '0.5'):
+    def init__(self, robot: RobotInterface, log_info:str = '', store_freq:float = 0.5):
         super().__init__(log_info, store_freq)
         self.robot = robot
 
     def run(self):
-        raise NotImplementedError
+        start_time = datetime.now()
+        robot_timestamp_current = self.robot.get_robot_state()["timestamp"]
+        interval_start = robot_timestamp_current
+        current_split = list()
+        split_cnt = 0
+        while not self._stop_event.is_set():
+            log_delay = 1/self.store_freq * 1000 # in ms
+            robot_timestamp_current = self.robot.get_robot_state()["timestamp"]
+            if self._split_event.is_set():
+                self._split_event.clear()
+                log_name = f"{start_time.strftime('%Y-%m-%d_%H-%M-%S')}_{self.log_info}_SPLIT_{split_cnt}"
+                self._store_data(current_split, log_name)
+                split_cnt += 1
+                interval_start = robot_timestamp_current
+                current_split = list()
+            if robot_timestamp_current >= interval_start + log_delay:
+                current_split += self.robot._get_robot_state_log((interval_start, robot_timestamp_current))
+                log_name = f"{start_time.strftime('%Y-%m-%d_%H-%M-%S')}_{self.log_info}_SPLIT_{split_cnt}"
+                self._store_data(current_split, log_name)
