dca5e31b096ae42f047b3b34a4608f59bc1ed941
ploedige <59517210+ploedige@users.noreply.github.com>
2024-03-25 12:21:13 +0100

    added initialization step to torque demonstration

--- tasks/torque_demonstration_replay.py
+++ tasks/torque_demonstration_replay.py
@@ -2,12 +2,18 @@ from controllers.torque_trajectory_executor import TorqueTrajectoryExecutor
 from tasks.base_tasks import ReplayBaseTask
 from torchcontrol.utils.tensor_utils import to_tensor
 
+from torchcontrol.policies.impedance import HybridJointImpedanceControl
+
 class TorqueDemonstrationReplay(ReplayBaseTask):
     def __init__(self, robots, demonstrations):
         super().__init__(robots, demonstrations)
         self.demonstration = self.demonstrations[0]
-        self.torque_trajectory = [tp.motor_torques_measured for tp in self.demonstration]
-        self.torque_trajectory = to_tensor(self.torque_trajectory)
+        self.torque_trajectory = [tp.joint_torques_computed for tp in self.demonstration]
+        self.torque_trajectory = [to_tensor(tp) for tp in self.torque_trajectory]
+        self.pos_trajectory = [tp.joint_positions for tp in self.demonstration]
+        self.pos_trajectory = [to_tensor(tp) for tp in self.pos_trajectory]
+        for robot in self.robots:
+            robot.move_to_joint_positions(self.pos_trajectory[0])
 
     def run(self):
         for robot in self.robots:
